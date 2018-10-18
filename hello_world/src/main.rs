d345aa56e78c4c0246190e630b98ea670d1221e5
Henry K <mykim1891@hotmail.com>
2018-10-17 20:16:30 -0700

    hello world and age

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // let name = "Jackson";
+    /* To specify the type of the variable */
+    let name: &str = "Jackson";
+    let mut age = 42; // this makes the variable mutable
+    age += 1;
+    println!("Hello, {}!", name);
 }
