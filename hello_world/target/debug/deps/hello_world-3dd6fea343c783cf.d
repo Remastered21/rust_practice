71dd320e14d5937327233385a7fd8bd65f0a9d79
Henry K <mykim1891@hotmail.com>
2018-10-17 17:25:58 -0700

    Rust Init

---
+++ hello_world/target/debug/deps/hello_world-3dd6fea343c783cf.d
@@ -0,0 +1,5 @@
+C:\Users\mykim\Desktop\my_projects\rust_practice\hello_world\target\debug\deps\hello_world-3dd6fea343c783cf.exe: src\main.rs
+
+C:\Users\mykim\Desktop\my_projects\rust_practice\hello_world\target\debug\deps\hello_world-3dd6fea343c783cf.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\mykim\Desktop\my_projects\rust_practice\hello_world\target\debug\hello_world.pdb: C:\Users\mykim\Desktop\my_projects\rust_practice\hello_world\src\main.rs
